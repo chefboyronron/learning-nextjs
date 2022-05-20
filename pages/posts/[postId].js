d1c682b660873d77aa2a0f10f390f25a9737f0f1
chefboyronron <rgpseminiano@gmail.com>
2022-05-20 17:20:47 +0800

    Static Site Generation (SSG) with Dynamic Parameters

---
+++ pages/posts/[postId].js
@@ -0,0 +1,40 @@
+function Post({ post }) {
+    return (
+        <div>
+            <h1>{post.id} {post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    )
+}
+
+export async function getStaticPaths() {
+    return {
+        // data is sliced into 3 in the posts index page
+        paths: [
+            {
+                params: { postId: '1'}
+            },
+            {
+                params: { postId: '2'}
+            },
+            {
+                params: { postId: '3'}
+            }
+        ],
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
+    const data = await response.json();
+
+    return {
+        props: {
+            post: data
+        }
+    }
+}
+
+export default Post;
---
+++ pages/posts/index.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+function PostList( {posts} ) {
+    return (
+        <div>
+            {
+                posts.map((post) => {
+                    return(
+                        <div key={post.id}>
+                            <h2>
+                                <Link href={`/posts/${post.id}`}>
+                                    <a>{post.id} {post.title}</a>
+                                </Link>
+                            </h2>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+
+    return {
+        props: {
+            posts: data.slice(0, 3)
+        }
+    }
+}
+
+export default PostList;
