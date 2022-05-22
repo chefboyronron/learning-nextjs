c981439710f115cfcbd740d01a04916c656ed1cf
chefboyronron <rgpseminiano@gmail.com>
2022-05-22 13:42:40 +0800

    SASS Support

--- pages/styling/about.js
+++ pages/styling/about.js
@@ -1,7 +1,11 @@
-import styles from '../../styles/About.module.css';
+import styles from '../../styles/About.module.scss';
 
 function About() {
-    return <div className={styles.highlight}>About Page</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.highlightscss}>About Page</div>
+        </div>
+    )
 }
 
 export default About;
