fb808b175b388b27e745aed59b80c2a9f5fe7e41
chefboyronron <rgpseminiano@gmail.com>
2022-05-22 12:53:35 +0800

    Global Style - ex with Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+// npm install bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/styling/about.js
@@ -0,0 +1,9 @@
+function About() {
+    return(
+        <>
+            <button className='btn btn-success'>Primary</button>
+        </>
+    )
+}
+
+export default About;
