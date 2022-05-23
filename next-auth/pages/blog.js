00b078cc59da6a3d9969156fe110abb747741e68
chefboyronron <rgpseminiano@gmail.com>
2022-05-23 18:09:57 +0800

    Securing Pages Server Side

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,5 +1,28 @@
-function Blog() {
-    return <h1>Dashboard Page</h1>
+import { getSession, useSession } from 'next-auth/client'
+
+function Blog({ data }) {
+  const [session] = useSession()
+  console.log({ session })
+
+  return <h1>Blog page - {data}</h1>
 }
 
-export default Blog;
+export default Blog
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin?callbackUrl=http://localhost:3000/blog',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {
+      data: session ? 'List of 100 personalized blogs' : 'List of free blogs',
+      session
+    }
+  }
+}
